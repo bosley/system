28d7e5808d812211cb916d24e7d20579d84bbb84
bosley <bosley117@gmail.com>
2022-02-04 15:31:06 +0000

    Deleted duplicate dir
    added install script

---
+++ setup_mac.sh
@@ -0,0 +1,30 @@
+cwd=$(pwd)
+
+echo "Installing brew"
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing goodies"
+
+# Install goodies
+brew install git zsh htop python3 neovim tmux tree
+
+echo "Installing oh my zsh"
+
+# OH MY ZSH!
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd $cwd
+
+echo "Installing configs"
+
+# Install configs
+/bin/bash/ install.sh
+
+echo "Source"
+
+# Source 
+source ~/.zshrc
+
+echo "Complete"
